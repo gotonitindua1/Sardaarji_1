a11fc4d2eb80b8073b10ab5c51d8c85dd6a26e50
gotonitindua1 <gotonitindua1@gmail.com>
2025-03-28 08:50:03 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 let food1s = document.getElementById('food2');
-let food2s = document.getElementById('food2');
+let food2s = document.getElementById('food3');
 let foods = document.getElementById('food');
 
 food1s.addEventListener('click',()=>{
