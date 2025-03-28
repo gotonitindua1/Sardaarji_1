ac58dfca279425d7e8b3e110f29fdde0a89d5ea4
gotonitindua1 <gotonitindua1@gmail.com>
2025-03-28 08:45:06 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-let food1s = document.getElementById('food1');
+let food1s = document.getElementById('food2');
 let food2s = document.getElementById('food2');
 let foods = document.getElementById('food');
 
@@ -7,5 +7,5 @@ food1s.addEventListener('click',()=>{
 })
 
 food2s.addEventListener('click',()=>{
-    foods.style.backgroundImage="url('food.jpg')";
+    foods.style.backgroundImage="url('food2.jpg')";
 })
